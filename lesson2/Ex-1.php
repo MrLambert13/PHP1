bfcd28f35abaeb33f8b00d8625d30988fabe824a
Mr.L@mbert_13 <lam_13@mail.ru>
2018-10-09 11:10:18 +0300

    Ex2 completed

--- lesson2/Ex-1.php
+++ lesson2/Ex-1.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 09.10.2018
- * Time: 9:29
- */
+
+function compareDigits(int $a, int $b){
+  $result = null;
+  if ($a >= 0 && $b >= 0) {
+    $result = $a - $b;
+  } else if ($a < 0 && $b < 0) {
+    $result = $a * $b;
+  } else $result = $a + $b;
+
+  return $result;
+}
+
+echo compareDigits(13, 14) . "\n";  // -1
+echo compareDigits(5, -3) . "\n";   // 2
+echo compareDigits(-2, -7);         // 14
+
+
