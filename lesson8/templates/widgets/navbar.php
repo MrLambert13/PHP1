00c235ea334e98ae5174eec21d96793108524e7f
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-10-31 01:01:55 +0300

    navbar

--- lesson8/templates/widgets/navbar.php
+++ lesson8/templates/widgets/navbar.php
@@ -18,7 +18,7 @@ $items = [
         <img src="/img/logo.png" alt="<?= $config['name'] ?>" height="30" class="mr-2">
         <?= $config['name'] ?>
     </a>
-    <div class="collapse navbar-collapse navbar-right" id="navbarNav">
+    <div class=" navbar-collapse navbar-right" id="navbarNav">
         <ul class="navbar-nav ml-auto">
             <?php foreach ($items as $link) : ?>
                 <?php if (isset($link['role'])): ?>
